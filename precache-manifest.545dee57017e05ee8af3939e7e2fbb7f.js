f18249e1af2684b7c479b4c94e1e9a82ef20483a
Humphrey Tom <humphreytom14@gmail.com>
2020-12-06 22:45:33 +0300

    Updates

---
+++ precache-manifest.545dee57017e05ee8af3939e7e2fbb7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76e983ce33f80058d8646a6eb7456f7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2475f26efe718ea06ebf",
+    "url": "/portfolio/static/css/main.f77d7a38.chunk.css"
+  },
+  {
+    "revision": "d516b79519b6fa620c53",
+    "url": "/portfolio/static/js/2.67ecf193.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/portfolio/static/js/2.67ecf193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2475f26efe718ea06ebf",
+    "url": "/portfolio/static/js/main.9f3b1116.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ee6e96f76622e03596a74f4353553255",
+    "url": "/portfolio/static/media/your photo.ee6e96f7.JPG"
+  }
+]);
